8d21a88c92c433295d7c17af1ea61ff51d33cb94
Mohit Bhasi <mohitbhasi1998@gmail.com>
2020-07-02 20:26:52 +0530

    Project models added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import datetime
-from account import models as account_models
-from project import models as project_models
+from account.models import SIG, User, Core
+from project.models import Project
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.core.management import call_command
@@ -12,25 +12,19 @@ import subprocess
 def indexView(request):
     this_year = datetime.now().year
     years_in_operation = this_year-1995
-    sigs = account_models.SIG.objects.all()
-    member_count = len(
-        account_models.User.objects.filter(
-            batch_of__in = [this_year+3, this_year+2, this_year+1]
-        )
-    )
-    sig_count=len(account_models.SIG.objects.all())
-    '''
-    project_count = len(
-        project_models.Project.objects.filter(
-            year = this_year
-        )
-    )
-    '''
-    core = None
+    sigs = SIG.objects.all()
+    member_count = User.objects.filter(
+        batch_of__in = [this_year+3, this_year+2, this_year+1]
+    ).count()
+    
+    sig_count = sigs.count()
+    project_count = Project.objects.filter(
+        year = this_year
+    ).count()
+    
     project_count = 20
-    member_count = 2000
-
-    #core = account_models.Core.objects.all()
+    member_count = User.objects.all().count()
+    core = Core.objects.all()
     context = {
         'core':core, 'sigs':sigs,
         'year':years_in_operation, 'member':member_count,
--- project/models.py
+++ project/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from account.models import SIG, User
+from datetime import datetime
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+def year_choices():
+    return [(r,r) for r in range(2018, datetime.today().year +1)]
+
+class Project(models.Model):
+    name = models.CharField(
+        default = "",
+        max_length = 200
+    )
+    tags = models.CharField(
+        default = "",
+        max_length = 200
+    )
+    members = models.ManyToManyField(User)
+    sig = models.ManyToManyField(SIG)
+    year = models.IntegerField(
+        choices = year_choices(),
+        default=datetime.today().year
+    )
+    description = RichTextUploadingField()
