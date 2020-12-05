3b4f46d1e394496ff36fb3a5877a48661136e410
AmodhShenoy <amodhshenoy@gmail.com>
2020-12-05 16:22:28 +0530

    Restructuring

---
+++ server-code/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/winstone/DRIVES/Work/ISTE/Website/iste.nitk.ac.in/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
