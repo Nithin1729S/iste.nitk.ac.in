cea3d65e319ccaa831550f32fa540ea569b8ca86
ArjunAnilPillai <arjunanilpillai@gmail.com>
2020-12-15 14:49:23 +0530

    Added axios, django-cors-headers, base code for fetching data

--- frontend-code/src/components/HomeComponent.js
+++ frontend-code/src/components/HomeComponent.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class HomeComponent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            homeData: [],
+        };
+        this.fetchHome = this.fetchHome.bind(this)
+    }
+    fetchHome() {
+        console.log("Fetching. . .")
+        axios.get('http://127.0.0.1:8000/home/')
+            .then(res => {
+                this.setState({
+                    homeData: res.data,
+                });
+                console.log(this.state.homeData);
+            })
+    }
+    componentDidMount(){
+        this.fetchHome();
+    }
     render(){
         return(
             <div>
