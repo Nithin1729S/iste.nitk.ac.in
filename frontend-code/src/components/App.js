a45ad13d5564d50b5efb39b5f66a0411ba02f621
Monit <guptamonit.2002@gmail.com>
2021-02-01 20:15:24 +0530

    Added Team Component

--- frontend-code/src/components/App.js
+++ frontend-code/src/components/App.js
@@ -8,7 +8,7 @@ import HomeComponent from './HomeComponent';
 import EventComponent from './EventComponent';
 import SigComponent from './SigComponent';
 import TeamComponent from './TeamComponent';
-import ProjectComponent from './ProjectComponent';
+//import ProjectComponent from './ProjectComponent';
 import '../css/constants.css';
 
 class App extends React.Component{
@@ -16,15 +16,13 @@ class App extends React.Component{
         return(
             <Router history={history}>
                 <Header/>
-                <main>
+                <div>
                     <Route path='/' exact component={HomeComponent}/>
                     <Route path='/event' exact component ={EventComponent}/>
                     <Route path='/sig' exact component={SigComponent}/>
-                    <Route path='/team' exact component={TeamComponent} />
-                    <Route path='/project' exact component={ProjectComponent}/>
-                 </main>
-                    
-                
+                    <Route path='/team' exact component={TeamComponent}/>
+                    {/* <Route path='/project' exact component={ProjectComponent}/> */}
+                </div>
                 <Footer/>
             </Router>
         );
