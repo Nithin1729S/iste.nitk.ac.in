c49869d7a29279efd5bf7ecf0d4c4b54b7296af1
AmodhShenoy <amodhshenoy@gmail.com>
2021-03-20 15:55:43 +0530

    Carousel model changed

--- backend-code/home/models.py
+++ backend-code/home/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+CAROUSEL_TYPES = (('1','Mobile'),
+                ('2','Desktop'))
+
 class Carousel(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(default="carousel_image", max_length=200)
     url = models.CharField(max_length=200)
+    type = models.CharField(choices=CAROUSEL_TYPES, max_length=1, default="2")
     
