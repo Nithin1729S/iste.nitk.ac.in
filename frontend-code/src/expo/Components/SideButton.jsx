698b34c14a21852ec94bf885c28d9f9e4ded97ba
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-03-24 02:29:13 +0530

    Added logo for sidenav

--- frontend-code/src/expo/Components/SideButton.jsx
+++ frontend-code/src/expo/Components/SideButton.jsx
@@ -1,43 +1,41 @@
-import React from "react";
-import { Link } from "react-scroll";
+import React from 'react';
+import { Link } from 'react-scroll';
 
-import styles from "../css/side.module.css";
-import logo from "../Assets/crypt.jpg";
-import { offsetCoefficient } from "../Assets/constants";
+import styles from '../css/side.module.css';
+import logo from '../Assets/crypt.jpg';
+import { offsetCoefficient } from '../Assets/constants';
 const SideButton = ({
-    name,
-    isLeft,
-    handleClick,
-    isActive,
-    projectId,
-    itemId,
+	name,
+	isLeft,
+	handleClick,
+	isActive,
+	projectId,
+	itemId,
+	logoSide,
 }) => {
-    const clickHandler = () => {
-        handleClick(itemId);
-    };
-    return (
-        <div
-            className={`${styles.main} ${isLeft ? styles.left : styles.right} ${
-                isActive ? styles.active : null
-            }`}
-        >
-            <Link
-                className={styles.link}
-                to={itemId + ""}
-                smooth
-                duration={
-                    500 *
-                    (projectId > itemId
-                        ? projectId - itemId
-                        : itemId - projectId)
-                }
-                offset={window.innerHeight * offsetCoefficient}
-                onClick={clickHandler}
-            >
-                <img src={logo} alt="" />
-                <h4>{name}</h4>
-            </Link>
-        </div>
-    );
+	const clickHandler = () => {
+		handleClick(itemId);
+	};
+	return (
+		<div
+			className={`${styles.main} ${isLeft ? styles.left : styles.right} ${
+				isActive ? styles.active : null
+			}`}
+		>
+			<Link
+				className={styles.link}
+				to={itemId + ''}
+				smooth
+				duration={
+					500 * (projectId > itemId ? projectId - itemId : itemId - projectId)
+				}
+				offset={window.innerHeight * offsetCoefficient}
+				onClick={clickHandler}
+			>
+				<img src={logoSide === '' ? logo : logoSide} alt="" />
+				<h4>{name}</h4>
+			</Link>
+		</div>
+	);
 };
 export default SideButton;
