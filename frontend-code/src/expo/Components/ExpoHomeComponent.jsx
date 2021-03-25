0e4b7a690c698a65e7df3e8f32f01b837e557393
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-03-26 02:31:10 +0530

    tags content added

--- frontend-code/src/expo/Components/ExpoHomeComponent.jsx
+++ frontend-code/src/expo/Components/ExpoHomeComponent.jsx
@@ -1,56 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import TopNav from './TopNav';
-import Projects from './Projects';
-import SideNav from './SideNav';
+import TopNav from "./TopNav";
+import Projects from "./Projects";
+import SideNav from "./SideNav";
 
-import styles from '../css/app.module.css';
-import { constant, sigNames } from '../Assets/constants';
+import styles from "../css/app.module.css";
+import { constant, sigNames } from "../Assets/constants";
+import { Link } from "react-router-dom";
 class ExpoHomeComponent extends React.Component {
-	componentDidMount() {
-		this.props.setHeaderFooterStatus(false);
-	}
-	state = { sig: 'catalyst', projectId: 1 };
+    componentDidMount() {
+        this.props.setHeaderFooterStatus(false);
+    }
+    state = { sig: "catalyst", projectId: 1 };
 
-	handleSigChange = (selectedSig) => {
-		this.setState({ sig: selectedSig, projectId: 1 });
-	};
+    handleSigChange = (selectedSig) => {
+        this.setState({ sig: selectedSig, projectId: 1 });
+    };
 
-	handleProjectChange = (selectedProject) => {
-		this.setState({ projectId: selectedProject });
-	};
+    handleProjectChange = (selectedProject) => {
+        this.setState({ projectId: selectedProject });
+    };
 
-	render() {
-		const { projectId, sig } = this.state;
-		return (
-			<div className={styles.main}>
-				<div className={styles.sideContent}>
-					<SideNav
-						handleClick={(project) => this.handleProjectChange(project)}
-						activeSig={sig}
-						projectId={projectId}
-						data={constant[sig]}
-					/>
-				</div>
-				<div className={styles.content}>
-					<div className={styles.topNav}>
-						<TopNav
-							sig={sig}
-							handleClick={(sig) => this.handleSigChange(sig)}
-							projectId={projectId}
-							data={sigNames}
-						/>
-					</div>
-					<div className={styles.projectContent}>
-						<Projects sig={sig} />
-					</div>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        const { projectId, sig } = this.state;
+        return (
+            <div className={styles.main}>
+                <div className={styles.sideContent}>
+                    <SideNav
+                        handleClick={(project) =>
+                            this.handleProjectChange(project)
+                        }
+                        activeSig={sig}
+                        projectId={projectId}
+                        data={constant[sig]}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.topNav}>
+                        <Link to="../" style={{ float: "right" }}>
+                            ISTE NITK
+                        </Link>
+                        <TopNav
+                            sig={sig}
+                            handleClick={(sig) => this.handleSigChange(sig)}
+                            projectId={projectId}
+                            data={sigNames}
+                        />
+                    </div>
+                    <div className={styles.projectContent}>
+                        <Projects sig={sig} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
-	componentWillUnmount() {
-		this.props.setHeaderFooterStatus(true);
-	}
+    componentWillUnmount() {
+        this.props.setHeaderFooterStatus(true);
+    }
 }
 export default ExpoHomeComponent;
--- frontend-code/src/expo/Components/ProjectContent.jsx
+++ frontend-code/src/expo/Components/ProjectContent.jsx
@@ -1,39 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import styles from '../css/project.module.css';
-import logo from '../Assets//crypt.jpg';
+import styles from "../css/project.module.css";
+import logo from "../Assets//crypt.jpg";
 const ProjectContent = ({ projectData, sig }) => {
-	console.log(projectData, sig);
-	var desc = projectData.oneline;
-	if (typeof desc === 'undefined') {
-		desc = 'Description.';
-	}
-
-	return (
-		<div className={styles.main} id={projectData.id}>
-			<div className={styles.imageContent}>
-				<img
-					alt="project"
-					src={
-						typeof projectData.logo === 'undefined' ? logo : projectData.logo
-					}
-					className={styles.image}
-				/>
-			</div>
-			<div className={styles.content}>
-				<h2 className={styles.header}>{projectData.name}</h2>
-				<div>{desc}</div>
-				<div className={styles.buttonContent}>
-					<Link
-						to={`/expo/${sig.concat(projectData.id)}`}
-						className="waves-light btn-small btnColor"
-					>
-						View Details
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+    console.log(projectData, sig);
+    var desc = projectData.oneline;
+    if (typeof desc === "undefined") {
+        desc = "Description.";
+    }
+    const renderingTags = projectData.tags.slice(0,6).map((item) => (
+        <div key={item}>{item}</div>
+    ));
+    return (
+        <div className={styles.main} id={projectData.id}>
+            <div className={styles.imageContent}>
+                <img
+                    alt="project"
+                    src={
+                        typeof projectData.logo === "undefined"
+                            ? logo
+                            : projectData.logo
+                    }
+                    className={styles.image}
+                />
+            </div>
+            <div className={styles.content}>
+                <h2 className={styles.header}>{projectData.name}</h2>
+                <div className={styles.tagContainer}>{renderingTags}</div>
+                <div>{desc}</div>
+                <div className={styles.buttonContent}>
+                    <Link
+                        to={`/expo/${sig.concat(projectData.id)}`}
+                        className="waves-light btn-small btnColor"
+                    >
+                        View Details
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default ProjectContent;
