e949d960755e5c39284aac7756b3ead97b49cc06
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-03-26 14:13:43 +0530

    further reading added

--- frontend-code/src/expo/Assets/further.js
+++ frontend-code/src/expo/Assets/further.js
@@ -1,55 +1,83 @@
 export const fur = {
-	catalyst: [
-		{ id: 1, fur: '' }, //Simulation Model for Biodiesel Production Embedded with MPC using PFR
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Modelling Of Waste Water Treatment Using ASM
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Photocatalytic Degradation of Aniline
-	],
-	charge: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //Implemention of CNNs on FPGA
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Eye Tracking Wheelchair
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Speech to Text Converter
-		{ id: 4, fur: 'http://localhost:3000/#/expo/' }, //Wireless Solar charger
-		{ id: 5, fur: 'http://localhost:3000/#/expo/' }, //Hardware Assisted SSL
-		{ id: 6, fur: 'http://localhost:3000/#/expo/' }, //Medical Image Segmentation
-	],
-	chronicle: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //The Way you Make me Feel : Background Score Analysis
-		{ id: 2, fur: '' }, //Podcast
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Short Film
-	],
-	clutch: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //Simulation of exhaust back pressure for engine braking
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Exhaust tuning of IC Enginez
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Energy efficient glider for delivery
-		{ id: 4, fur: 'http://localhost:3000/#/expo/' }, //Alloy Recommender System
-	],
-	concrete: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //Design of a Green Commercial Complex in NITK using BIM
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Design of Bridge Girders-Using Euro and AASHTO codes
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Pothole Detector
-		{ id: 4, fur: 'http://localhost:3000/#/expo/' }, //Design and seismic analysis of a building
-		{ id: 5, fur: 'http://localhost:3000/#/expo/' }, //Wind Load Analysis of Building with Different Shear Wall Designs
-	],
-	create: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //Long Exposure Photography
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Short Film
-	],
-	credit: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //Analysis of Money Laundering in India
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //Importance of Financial Education and techniques for its effective implementation
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Economics of Space Commercialization
-		{ id: 4, fur: 'http://localhost:3000/#/expo/' }, //Effective marketing strategies and study of consumer behaviour in the beverage industry
-		{ id: 5, fur: 'http://localhost:3000/#/expo/' }, //Analysis of 5 Major Schemes in the Indian Apparels and Textile Industry
-		{ id: 6, fur: 'http://localhost:3000/#/expo/' }, //Analysis on the Effectiveness of Candlestick Patterns
-		{ id: 7, fur: 'http://localhost:3000/#/expo/' }, //Sentiments of Workers in the Gig Economy
-	],
-	crypt: [
-		{ id: 1, fur: 'http://localhost:3000/#/expo/' }, //NDN Reddit
-		{ id: 2, fur: 'http://localhost:3000/#/expo/' }, //CI/CD pipeline using K8
-		{ id: 3, fur: 'http://localhost:3000/#/expo/' }, //Voice Navigator
-		// { id: 4, fur: 'http://localhost:3000/#/expo/' }, //Dank Learning
-		{ id: 5, fur: 'http://localhost:3000/#/expo/' }, //Python web application honey pot
-		{ id: 6, fur: 'http://localhost:3000/#/expo/' }, //B&W Image Colorization Using GANs
-		{ id: 7, fur: 'http://localhost:3000/#/expo/' }, //SketchGen
-	],
+    catalyst: [
+        {
+            id: 1,
+            fur:
+                "https://drive.google.com/file/d/1ux45OS5ShK1JwD6EkzAyxbYdu7HqBxxY/view?usp=sharing",
+        }, //Simulation Model for Biodiesel Production Embedded with MPC using PFR
+        {
+            id: 2,
+            fur:
+                "https://drive.google.com/file/d/1HoLEc9bACnJz7tapoVIU_GBGdV7utNQi/view?usp=sharing",
+        }, //Modelling Of Waste Water Treatment Using ASM
+        {
+            id: 3,
+            fur:
+                "https://drive.google.com/file/d/1Z98o7SG-Wv-FAWgggt_Lk01QWnyH7-9i/view?usp=sharing",
+        }, //Photocatalytic Degradation of Aniline
+    ],
+    charge: [
+        { id: 1, fur: "https://github.com/putlurusurya/CNNOnFPGA" }, //Implemention of CNNs on FPGA
+        {
+            id: 2,
+            fur:
+                "https://drive.google.com/file/d/1yA5zIlMviWrBNVIPFFwfcavt734Ve3jk/view?usp=sharing",
+        }, //Eye Tracking Wheelchair
+        {
+            id: 3,
+            fur:
+                "https://drive.google.com/file/d/1y556F2Kbk3RztTPTDxFx17dw47ySoBNZ/view?usp=sharing",
+        }, //Speech to Text Converter
+        {
+            id: 4,
+            fur:
+                "https://drive.google.com/file/d/1y556F2Kbk3RztTPTDxFx17dw47ySoBNZ/view?usp=sharing",
+        }, //Wireless Solar charger
+        { id: 5 }, //Hardware Assisted SSL
+        {
+            id: 6,
+            fur:
+                "https://drive.google.com/file/d/1Y_jGLl7MpoLOFHvpnP5mtOkUzklAx6He/view?usp=sharing",
+        }, //Medical Image Segmentation
+    ],
+    chronicle: [
+        { id: 1 }, //The Way you Make me Feel : Background Score Analysis
+        { id: 2 }, //Podcast
+        { id: 3 }, //Short Film
+    ],
+    clutch: [
+        { id: 1, fur: "https://drive.google.com/file/d/1oyqopD9o2AKGEkcGflwZ3_CfcE6mfEkO/view?usp=sharing" }, //Simulation of exhaust back pressure for engine braking
+        { id: 2, fur: "https://drive.google.com/file/d/1VS8h_s3UlXyo4-TzM1btLs-Bl-Ddacta/view?usp=sharing" }, //Exhaust tuning of IC Enginez
+        { id: 3, fur: "https://drive.google.com/file/d/1vT5wJzp6mtssKghKVa_FTNxnZUpKdecY/view?usp=sharing" }, //Energy efficient glider for delivery
+        { id: 4, fur: "https://drive.google.com/file/d/10M64uHFBU8pZuZyDv_L1vh12dDUQsKws/view?usp=sharing" }, //Alloy Recommender System
+    ],
+    concrete: [
+        { id: 1, fur: "https://drive.google.com/file/d/1cMiEuBDLXdhCWahKse5xNdnz-U6zXFG-/view?usp=sharing" }, //Design of a Green Commercial Complex in NITK using BIM
+        { id: 2 }, //Design of Bridge Girders-Using Euro and AASHTO codes
+        { id: 3, fur: "https://drive.google.com/file/d/1OtKDw-GBqicTfNVE_0v801mOXO4PyLJS/view?usp=sharing" }, //Pothole Detector
+        { id: 4, fur: "https://drive.google.com/file/d/1OwPaRoW7vXXjyTiw3ACusspWiNs0KC5d/view?usp=sharing" }, //Design and seismic analysis of a building
+        { id: 5, fur: "https://drive.google.com/file/d/1U5Y-WdzZMS86uH3yUwrg7klP2ENMSHym/view?usp=sharing" }, //Wind Load Analysis of Building with Different Shear Wall Designs
+    ],
+    create: [
+        { id: 1 }, //Long Exposure Photography
+        { id: 2 }, //Short Film
+    ],
+    credit: [
+        { id: 1, fur: "https://drive.google.com/file/d/1gFijRFk2T0pGnouOSKSFrYDrtZ4XIVzR/view?usp=sharing" }, //Analysis of Money Laundering in India
+        { id: 2, fur: "https://drive.google.com/file/d/10Ez5RDEI756HueX-c_uSmgOKVIs7RAzj/view?usp=sharing" }, //Importance of Financial Education and techniques for its effective implementation
+        { id: 3, fur: "https://drive.google.com/file/d/1VSbFSooasiHs4npCVUQPD3yy_zJmAqXE/view?usp=sharing" }, //Economics of Space Commercialization
+        { id: 4, fur: "https://drive.google.com/file/d/1DJ45Cz-3WH9LbYfmfwI63fdG-V-S2D_D/view?usp=sharing" }, //Effective marketing strategies and study of consumer behaviour in the beverage industry
+        { id: 5, fur: "https://drive.google.com/file/d/11dlk1_2J7hOENtA-YzoH5vAAEDJl5QRW/view?usp=sharing" }, //Analysis of 5 Major Schemes in the Indian Apparels and Textile Industry
+        { id: 6, fur: "https://drive.google.com/file/d/1qgmTPXfLKcEyIH-_lP8YmXNA6WSgOGYz/view?usp=sharing" }, //Analysis on the Effectiveness of Candlestick Patterns
+        { id: 7, fur: "https://drive.google.com/file/d/1sNoCw5iv0EJ8acQ1u_4WboDtfrerlz6E/view?usp=sharing" }, //Sentiments of Workers in the Gig Economy
+    ],
+    crypt: [
+        { id: 1, fur: "https://github.com/ArjunAnilPillai/ISTE-NDN-Reddit-" }, //NDN Reddit
+        { id: 2, fur: "https://github.com/Chinmayi27/ci-cd-pipeline" }, //CI/CD pipeline using K8
+        { id: 3, fur: "https://github.com/anshuman03sinha/Voice-Navigator" }, //Voice Navigator
+        // { id: 4, fur: 'https://github.com/hrithikbhat019/ISTE-Meme-Captioning-using-NNs' }, //Dank Learning
+        { id: 5, fur: "http://localhost:3000/#/expo/" }, //Python web application honey pot
+        { id: 6, fur: "https://github.com/mansivsaxena/BW-Image-Colorisation-using-GANs" }, //B&W Image Colorization Using GANs
+        { id: 7, fur: "https://github.com/Jagadish2000/Sketch_gen" }, //SketchGen
+    ],
 };
