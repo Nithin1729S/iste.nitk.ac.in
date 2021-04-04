224a649823bb43f4a305bca4f392c032dfcd6444
PriyankaPeri <priyankaperi.181ec233@nitk.edu.in>
2021-04-04 14:44:06 +0530

    QuestionItem completed

--- frontend-code/src/components/RenderingComponents/QuestionItem.js
+++ frontend-code/src/components/RenderingComponents/QuestionItem.js
@@ -1,8 +1,25 @@
 import React from "react";
+import styles from "../../css/QuestionItem.module.css";
 
 class QuestionItem extends React.Component {
-    render() {
-        return <div>Hello QuestionItem</div>;
-    }
+  anyname=()=>{console.log("anystring")};
+  render() {
+    const {
+      questionId,
+      status,
+      Title,
+      description,
+      points,
+      url,
+      hint1Url,
+      hint2Url,
+    } = this.props.item;
+    return (
+      <div onClick={this.anyname} className={`${styles.box} ${status?styles.attempted:null}`}>
+        <h3>{Title}</h3>
+        <h5>{points}</h5>
+      </div>
+    );
+  }
 }
 export default QuestionItem;
