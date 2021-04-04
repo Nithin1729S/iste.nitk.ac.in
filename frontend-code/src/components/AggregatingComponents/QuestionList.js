d365c495114b0dedaa06d5eccfa28988d42278f3
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-04-04 13:33:53 +0530

    questionList skeleton

--- frontend-code/src/components/AggregatingComponents/QuestionList.js
+++ frontend-code/src/components/AggregatingComponents/QuestionList.js
@@ -1,8 +1,48 @@
 import React from "react";
 
+import "../RenderingComponents/QuestionItem";
+import QuestionItem from "../RenderingComponents/QuestionItem";
+
+import styles from "../../css/QuestionList.module.css";
 class QuestionList extends React.Component {
+    // ** Question table: questionId, Title, description, points, url, hint1Url, hint2Url
+    data = [
+        {
+            questionId: 1,
+            Title: "",
+            description: "",
+            points: "",
+            url: "",
+            hint1Url: "",
+            hint2Url: "",
+        },
+        {
+            questionId: 2,
+            Title: "",
+            description: "",
+            points: "",
+            url: "",
+            hint1Url: "",
+            hint2Url: "",
+        },
+        {
+            questionId: 3,
+            Title: "",
+            description: "",
+            points: "",
+            url: "",
+            hint1Url: "",
+            hint2Url: "",
+        },
+    ];
+    componentDidMount() {
+        //TODO: replace with api request
+    }
+
+    QuestionItems = this.data.map((item) => <QuestionItem item={item} />);
+
     render() {
-        return <div>Hello QuestionList</div>;
+        return <div className={styles.main}>{this.QuestionItems}</div>;
     }
 }
 export default QuestionList;
