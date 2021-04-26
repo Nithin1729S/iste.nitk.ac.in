cfd7a14770c8e40842d494049861252dbe13c53f
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-04-26 13:09:55 +0530

    Cards for transcend added

--- frontend-code/src/components/Transcend/components/EventCards.js
+++ frontend-code/src/components/Transcend/components/EventCards.js
@@ -1,8 +1,40 @@
-const EventCards =()=>{
-    return(
-        <div>
-            Hello EventCards
+import styles from "../css/EventCards.module.css";
+
+const Card = ({ title, desc, imgUrl, formLink }) => {
+    return (
+        <div className={styles.card}>
+            <img src={imgUrl} alt={title} />
+            <h1>{title}</h1>
+            <p>{desc}</p>
+            <a href={formLink} className="indigo dark button">
+                View More
+            </a>
         </div>
     );
-}
+};
+
+const EventCards = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.a}>
+                <Card />
+            </div>
+            <div className={styles.c}>
+                <Card />
+            </div>
+            <div className={styles.b}>
+                <Card />
+            </div>
+            <div className={styles.d}>
+                <Card />
+            </div>
+            <div className={styles.e}>
+                <Card />
+            </div>
+            <div className={styles.f}>
+                <Card />
+            </div>
+        </div>
+    );
+};
 export default EventCards;
--- frontend-code/src/components/Transcend/components/TechCards.js
+++ frontend-code/src/components/Transcend/components/TechCards.js
@@ -3,11 +3,12 @@ import styles from "../css/TechCards.module.css";
 const TechCard = ({ title, desc, imgUrl }) => {
     return (
         <div className={styles.card}>
-            <div className={styles.front}>
-                <h3>{title}</h3>
-            </div>
+            <img src={imgUrl} alt={title} />
             <div className={styles.back}>
                 <p>{desc}</p>
+            </div>
+            <div className={styles.front}>
+                <h3>{title}</h3>
             </div>
         </div>
     );
@@ -17,7 +18,12 @@ const TechCards = ({ TechData }) => {
     return (
         <div className={styles.main}>
             {TechData.map(({ title, desc, imgUrl }) => (
-                <TechCard title={title} desc={desc} imgUrl={imgUrl} />
+                <TechCard
+                    key={title}
+                    title={title}
+                    desc={desc}
+                    imgUrl={imgUrl}
+                />
             ))}
         </div>
     );
