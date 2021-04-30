042bd0c2a77073ed8d29fca6e1391998cc2db974
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-04-30 11:41:51 +0530

    Constants updated for events

--- frontend-code/src/components/Transcend/Constants/EventCards/index.js
+++ frontend-code/src/components/Transcend/Constants/EventCards/index.js
@@ -1,5 +1,5 @@
 const eventData = [
-    { title: "a", desc: "asdas", imgUrl: "", formLink: "" },
+    { title: "Cryptonite", desc: "asdas", imgUrl: "", formLink: "" },
     { title: "b", desc: "asfga", imgUrl: "", formLink: "" },
     { title: "c", desc: "sdgadgasdf", imgUrl: "", formLink: "" },
     { title: "a", desc: "asdas", imgUrl: "", formLink: "" },
