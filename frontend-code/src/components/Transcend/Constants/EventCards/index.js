4f69f63eec5ee914b27e97bc4930439fee866246
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-04-30 20:06:25 +0530

    Added cards desktop view

--- frontend-code/src/components/Transcend/Constants/EventCards/index.js
+++ frontend-code/src/components/Transcend/Constants/EventCards/index.js
@@ -1,42 +1,49 @@
+import matlab from "../images/EventCards/Matlab.jpeg";
+import autocad from "../images/EventCards/AutocadW.jpeg";
+import premiere from "../images/EventCards/PremLoop.jpeg";
+import aero from "../images/EventCards/LiftOff.jpeg";
+import micro from "../images/EventCards/HCTF.jpeg";
+import oops from "../images/EventCards/Cryptonite.jpeg";
+
 const eventData = [
     {
         title: "MATLAB-Simulink Workshop",
         desc:
             "A workshop based on MATLAB-Simulink, a software essential for engineers to replicate and simulate processes throughout one's journey in college. This workshop will explain the fundamentals and rudiments of the software so that students are familiarized and well-versed with the platform's outlook. ",
-        img: "",
+        img: matlab,
         formLink: "https://forms.gle/pxiawRoVVXixs8HZ7",
     },
     {
         title: "Hardware CTF",
         desc:
             "A 24-hour CTF event involving a series of challenges related to microprocessors, signal and image processing, analog and digital electronics.",
-        img: "",
+        img: micro,
         formLink: "https://forms.gle/D43gezFgybNyn9tp6",
     },
     {
         title: "Lift Off",
         desc: "Placeholder content till we get the proper description",
-        img: "",
+        img: aero,
         formLink: "https://forms.gle/T2Xdgdv7yhgmoTv5A",
     },
     {
         title: "AutoCAD Workshop",
         desc:
             "Get them familiar with the basics of AutoCad including drawing shapes, performing editing commands, dimensioning, labelling etc. ",
-        img: "",
+        img: autocad,
         formLink: "https://forms.gle/shZw5TwxiGc82RTa8",
     },
     {
         title: "Create event",
         desc: "Teaching people how to do reels transitions professionally",
-        img: "",
+        img: premiere,
         formLink: "https://forms.gle/seMEqWxqcCNKeoAi6",
     },
     {
         title: "Cryptonite",
         desc:
             "A design only mini-hackathon catered towards first years to provide an experience towards hackathons without the underlying tech needed",
-        img: "",
+        img: oops,
         formLink: "https://forms.gle/aZRqn4NLxdyRowvW6",
     },
 ];
--- frontend-code/src/components/Transcend/components/EventCards.js
+++ frontend-code/src/components/Transcend/components/EventCards.js
@@ -1,23 +1,25 @@
 import styles from "../css/EventCards.module.css";
 
-const Card = ({ title, desc, img, formLink }) => {
+const Card = ({ title, desc, img, formLink, posIndex }) => {
+    var card = {0:styles.cardleft, 1:styles.cardleft, 2:styles.cardtop, 3:styles.cardtop, 4:styles.cardright, 5:styles.cardright};
+    var cardImg = {0:styles.cardimgleft, 1:styles.cardimgleft, 2:styles.cardimgtop, 3:styles.cardimgtop, 4:styles.cardimgright, 5:styles.cardimgright};
     return (
-        <div className={styles.card}>
-            <img src={img} alt={title} />
-            <h1>{title}</h1>
+        <div className={card[posIndex]}>
+            <img className={cardImg[posIndex]} src={img} alt={title} />
+            <div className={styles.cardesc}><h1>{title}</h1>
             <p>{desc}</p>
             <a href={formLink} target="_blank" rel="noreferrer">
                 <button className="waves-effect btn">
                     Click here to register
                 </button>
-            </a>
+            </a></div>
         </div>
     );
 };
 
 const EventCards = ({ EventData }) => {
-    const EventCards = EventData.map(({ title, desc, img, formLink }) => (
-        <Card title={title} img={img} desc={desc} formLink={formLink} />
+    const EventCards = EventData.map(({ title, desc, img, formLink}, index) => (
+        <Card title={title} img={img} desc={desc} formLink={formLink} posIndex={index}/>
     ));
     return (
         <div className={styles.main}>
