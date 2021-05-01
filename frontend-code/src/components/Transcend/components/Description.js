7243cedd00e4ff75715e6a7d64228174f856c381
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-05-01 10:23:15 +0530

    Final changes transcend

--- frontend-code/src/components/Transcend/components/Description.js
+++ frontend-code/src/components/Transcend/components/Description.js
@@ -1,6 +1,6 @@
 import styles from "../css/Description.module.css";
 
-const Description = ({ title, desc, img, logo }) => {
+const Description = ({ title,  img }) => {
     return (
         <div className={styles.main}>
             <img src={img} alt={title} className={styles.backgroundImage} />
