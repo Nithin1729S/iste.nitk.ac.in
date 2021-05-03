2d4762446e5d2452ad77368496b975caedc21ab6
aditya-rastogi23 <adit.rastogi2014@gmail.com>
2021-05-03 15:35:51 +0530

    Leaderboard API added

--- frontend-code/src/components/PageComponents/LeaderboardComponent.js
+++ frontend-code/src/components/PageComponents/LeaderboardComponent.js
@@ -3,174 +3,14 @@ import { Link } from "react-router-dom";
 import LeaderboardItem from "../RenderingComponents/LeaderboardItem";
 
 import styles from "../../css/leaderboard.module.css";
-
+import { baseRequest } from "../../constants";
 class LeaderboardComponent extends React.Component {
-    state = { searchInput: "", selectedFilter: "rollNum" };
-
-    LeaderboardElementsUnsorted = [
-        {
-            name: "test10",
-            rollNum: "10",
-            score: 250,
-            rank: "1",
-        },
-        {
-            name: "test1",
-            rollNum: "1",
-            score: 240,
-            rank: "1",
-        },
-        {
-            name: "test2",
-            rollNum: "2",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test3",
-            rollNum: "3",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test4",
-            rollNum: "4",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test5",
-            rollNum: "5",
-            score: 175,
-            rank: "3",
-        },
-        {
-            name: "test6",
-            rollNum: "6",
-            score: 120,
-            rank: "8",
-        },
-        {
-            name: "test7",
-            rollNum: "7",
-            score: 130,
-            rank: "7",
-        },
-        {
-            name: "test8",
-            rollNum: "8",
-            score: 140,
-            rank: "6",
-        },
-        {
-            name: "test10",
-            rollNum: "10",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test1",
-            rollNum: "1",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test2",
-            rollNum: "2",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test3",
-            rollNum: "3",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test4",
-            rollNum: "4",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test5",
-            rollNum: "5",
-            score: 175,
-            rank: "3",
-        },
-        {
-            name: "test6",
-            rollNum: "6",
-            score: 120,
-            rank: "8",
-        },
-        {
-            name: "test7",
-            rollNum: "7",
-            score: 130,
-            rank: "7",
-        },
-        {
-            name: "test8",
-            rollNum: "8",
-            score: 140,
-            rank: "6",
-        },
-        {
-            name: "test10",
-            rollNum: "10",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test1",
-            rollNum: "1",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test2",
-            rollNum: "2",
-            score: 200,
-            rank: "1",
-        },
-        {
-            name: "test3",
-            rollNum: "3",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test4",
-            rollNum: "4",
-            score: 150,
-            rank: "4",
-        },
-        {
-            name: "test5",
-            rollNum: "5",
-            score: 175,
-            rank: "3",
-        },
-        {
-            name: "test6",
-            rollNum: "6",
-            score: 120,
-            rank: "8",
-        },
-        {
-            name: "test7",
-            rollNum: "7",
-            score: 130,
-            rank: "7",
-        },
-        {
-            name: "test8",
-            rollNum: "8",
-            score: 140,
-            rank: "6",
-        },
-    ];
+    state = { searchInput: "", selectedFilter: "roll_number", elements: [] };
+    componentDidMount() {
+        baseRequest.get("/ctf/leaderboard/").then((res) => {
+            this.setState({ elements: res.data.teamList });
+        });
+    }
 
     handleClick = (e) => {
         this.setState({ selectedFilter: e.target.id });
@@ -180,24 +20,40 @@ class LeaderboardComponent extends React.Component {
         this.setState({ searchInput: e.target.value });
     };
 
-    LeaderboardElements = this.LeaderboardElementsUnsorted.sort(
-        (a, b) => b.score - a.score
-    );
+    calcRank = (items) => {
+        items[0].rank = 1;
+        for (let i = 1; i < items.length; i++) {
+            let element = items[i];
+            element.rank =
+                items[i - 1].score === items[i].score
+                    ? items[i - 1].rank
+                    : items[i - 1].rank + 1;
+        }
+        return items;
+    };
 
     render() {
-        const LeaderboardData = this.LeaderboardElements.filter((item) =>
-            item[this.state.selectedFilter].includes(
-                this.state.searchInput.toLowerCase()
+        if (!this.state.elements[0]) return null;
+        const unRankedElements = [
+            ...this.state.elements.map(({ roll_number, team_name, score }) => {
+                return { roll_number, team_name, score };
+            }),
+        ];
+        const leaderboardElements = this.calcRank(unRankedElements);
+        const LeaderboardData = leaderboardElements
+            .filter((item) =>
+                item[this.state.selectedFilter].includes(
+                    this.state.searchInput.toLowerCase()
+                )
             )
-        )
             .slice(0, 15)
-            .map(({ name, rollNum, score, rank }, index) => {
+            .map(({ team_name, roll_number, score, rank }) => {
                 return (
                     <LeaderboardItem
-                        key={index}
+                        key={roll_number}
                         rank={rank}
-                        name={name}
-                        rollNum={rollNum}
+                        name={team_name}
+                        rollNum={roll_number}
                         score={score}
                     />
                 );
@@ -218,23 +74,23 @@ class LeaderboardComponent extends React.Component {
                 <label>Filter:</label>
                 <button
                     className={
-                        this.state.selectedFilter === "rollNum"
+                        this.state.selectedFilter === "roll_number"
                             ? `${styles.button} ${styles.active}`
                             : styles.button
                     }
                     onClick={this.handleClick}
-                    id="rollNum"
+                    id="roll_number"
                 >
                     roll number
                 </button>
                 <button
                     className={
-                        this.state.selectedFilter === "name"
+                        this.state.selectedFilter === "team_name"
                             ? `${styles.button} ${styles.active}`
                             : styles.button
                     }
                     onClick={this.handleClick}
-                    id="name"
+                    id="team_name"
                 >
                     name
                 </button>
@@ -247,16 +103,14 @@ class LeaderboardComponent extends React.Component {
                     <div className={`${styles.header} center`}>
                         <h1>CTF LEADERBOARD</h1>
                         <Link to="/transcend/charge">
-                            <button className="btn">
-                                Back to Questions
-                            </button>
+                            <button className="btn">Back to Questions</button>
                         </Link>
                         <div className={`${styles.filter} ${styles.center}`}>
                             {searchParams}
                             <br />
                             <label htmlFor="search">
                                 Search by{" "}
-                                {this.state.selectedFilter === "rollNum"
+                                {this.state.selectedFilter === "roll_number"
                                     ? "roll number"
                                     : "name"}{" "}
                                 :
