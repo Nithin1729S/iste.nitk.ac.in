1e37823148c0a3087503083a22d2c95d48527fb9
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-08-03 21:50:38 +0530

    Added dummy data for recs

--- frontend-code/src/components/Recruitment/dummyData.js
+++ frontend-code/src/components/Recruitment/dummyData.js
@@ -1,27 +1,27 @@
 export const dummy = {
-	registerLink: 'google.com/',
+	regLink: 'google.com/',
 	rounds: [
 		{
 			roundName: 'Round 1',
-			date: 0,
+			date: '2021-05-19',
 			desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet turpis in diam elementum venenatis. Curabitur ut diam non ipsum euismod pharetra. Sed enim diam, euismod at nibh sit amet, consectetur maximus neque. Donec quis mauris nec libero eleifend suscipit blandit non metus. Nam risus dolor, faucibus eu lorem eget, egestas vulputate elit. Pellentesque sed nisi est. Suspendisse venenatis ullamcorper quam et mollis. Etiam nec turpis libero. Vivamus ac faucibus leo. Integer pulvinar, libero a accumsan rhoncus, lectus turpis consectetur libero, ac laoreet enim ex nec metus. ',
 			isOver: true,
 		},
 		{
 			roundName: 'Round 2',
-			date: 1,
+			date: '2021-05-21',
 			desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet turpis in diam elementum venenatis. Curabitur ut diam non ipsum euismod pharetra. Sed enim diam, euismod at nibh sit amet, consectetur maximus neque. Donec quis mauris nec libero eleifend suscipit blandit non metus. Nam risus dolor, faucibus eu lorem eget, egestas vulputate elit. Pellentesque sed nisi est. Suspendisse venenatis ullamcorper quam et mollis. Etiam nec turpis libero. Vivamus ac faucibus leo. Integer pulvinar, libero a accumsan rhoncus, lectus turpis consectetur libero, ac laoreet enim ex nec metus. ',
 			isOver: true,
 		},
 		{
 			roundName: 'Round 3',
-			date: 2,
+			date: '2021-05-23',
 			desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet turpis in diam elementum venenatis. Curabitur ut diam non ipsum euismod pharetra. Sed enim diam, euismod at nibh sit amet, consectetur maximus neque. Donec quis mauris nec libero eleifend suscipit blandit non metus. Nam risus dolor, faucibus eu lorem eget, egestas vulputate elit. Pellentesque sed nisi est. Suspendisse venenatis ullamcorper quam et mollis. Etiam nec turpis libero. Vivamus ac faucibus leo. Integer pulvinar, libero a accumsan rhoncus, lectus turpis consectetur libero, ac laoreet enim ex nec metus. ',
 			isOver: false,
 		},
 		{
 			roundName: 'Round 4',
-			date: 3,
+			date: '2021-05-25',
 			desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet turpis in diam elementum venenatis. Curabitur ut diam non ipsum euismod pharetra. Sed enim diam, euismod at nibh sit amet, consectetur maximus neque. Donec quis mauris nec libero eleifend suscipit blandit non metus. Nam risus dolor, faucibus eu lorem eget, egestas vulputate elit. Pellentesque sed nisi est. Suspendisse venenatis ullamcorper quam et mollis. Etiam nec turpis libero. Vivamus ac faucibus leo. Integer pulvinar, libero a accumsan rhoncus, lectus turpis consectetur libero, ac laoreet enim ex nec metus. ',
 			isOver: false,
 		},
