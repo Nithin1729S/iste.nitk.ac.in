1677fe0eb4fe24700921d780de3e259057d60986
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-08-08 20:13:19 +0530

    RegLink model registered

--- backend-code/recruitment/admin.py
+++ backend-code/recruitment/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Round
+from .models import RegLink, Round
 
 @admin.register(Round)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         'name','sig'
+    )
+
+@admin.register(RegLink)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'link', 'sig', 'descriptionSIG'
     )
