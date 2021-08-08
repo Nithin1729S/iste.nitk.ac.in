4094e0c2dfac2b9177f022ad6830b1093f979ac0
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-08-08 19:06:48 +0530

    Integration of timeline complete

--- frontend-code/src/components/Recruitment/components/Timeline.js
+++ frontend-code/src/components/Recruitment/components/Timeline.js
@@ -1,29 +1,26 @@
-import React from 'react'
-import '../css/Timeline.css'
-import {dummy} from '../dummyData'
+import React from 'react';
+import '../css/Timeline.css';
 const Timeline = (props) => {
-
-    const abc=dummy.rounds;
-    const listitems=abc.map((todo,index) => {
-        return (
-        <li key={index}>  
-        <span className="timeline-point"></span>
-        <span className="date">{todo.date}</span>
-        <p>{todo.roundName}</p>
-    </li>
-        )
-    })
-    return <div className="mainz">
-        <section className="timeline">
-            <div className="timeline-line">
-                {/* <span className="timeline-innerline"></span> */}
-            </div>
-            <ul>
-                {listitems}
-            </ul>
-        </section>
-       
-    </div>
-}
+	const abc = props.rounds;
+	const listitems = abc.map((todo, index) => {
+		return (
+			<li key={index}>
+				<span className="timeline-point"></span>
+				<span className="date">{todo.date}</span>
+				<p>{todo.roundName}</p>
+			</li>
+		);
+	});
+	return (
+		<div className="mainz">
+			<section className="timeline">
+				<div className="timeline-line">
+					{/* <span className="timeline-innerline"></span> */}
+				</div>
+				<ul>{listitems}</ul>
+			</section>
+		</div>
+	);
+};
 
 export default Timeline;
--- frontend-code/src/components/Recruitment/eachSIGLandingPage.jsx
+++ frontend-code/src/components/Recruitment/eachSIGLandingPage.jsx
@@ -1,66 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { dummy } from "./dummyData";
-import SigDescription from "./Components/SigDescription";
-import RegisterButton from "./Components/RegisterButton.jsx";
-import Timeline from './Components/Timeline';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { dummy } from './dummyData';
+import SigDescription from './components/SigDescription';
+import RegisterButton from './components/RegisterButton.jsx';
+import Timeline from './components/Timeline';
 
 class RecsSIGComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        var sigs = [
-            "Catalyst",
-            "Crypt",
-            "Create",
-            "Clutch",
-            "Chronicle",
-            "Charge",
-            "Concrete",
-            "Credit",
-        ];
-        let curSIGname = "Catalyst";
-        if (typeof this.props.match.params !== "undefined") {
-            let cur = this.props.match.params.name;
-            if (sigs.includes(cur)) curSIGname = cur;
-        }
-        this.state = {
-            sigName: curSIGname,
-            data: {
-                desc: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis quas corporis maiores vero aut nostrum tempora voluptatem pariatur expedita deserunt itaque debitis quae dolorum commodi in consequuntur, animi enim unde!",
-            },
-        };
-    }
+	constructor(props) {
+		super(props);
+		var sigs = [
+			'Catalyst',
+			'Crypt',
+			'Create',
+			'Clutch',
+			'Chronicle',
+			'Charge',
+			'Concrete',
+			'Credit',
+		];
+		let curSIGname = 'Catalyst';
+		if (typeof this.props.match.params !== 'undefined') {
+			let cur = this.props.match.params.name;
+			if (sigs.includes(cur)) curSIGname = cur;
+		}
+		this.state = {
+			sigName: curSIGname,
+			data: {
+				desc: ' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis quas corporis maiores vero aut nostrum tempora voluptatem pariatur expedita deserunt itaque debitis quae dolorum commodi in consequuntur, animi enim unde!',
+			},
+		};
+	}
 
-    //Do axios request and get data from backend.
-    // URL is /recs/
-    // Make sure to pass the SIG name, which is stored in this.state.sigName
-    // Use state variable data to get request from backend
+	//Do axios request and get data from backend.
+	// URL is /recs/
+	// Make sure to pass the SIG name, which is stored in this.state.sigName
+	// Use state variable data to get request from backend
 
-    // Components on this page
-    // 1. Timeline
-    // 2. Cards (Take from an earlier implementation if possible, else build from scratch)
-    // 3. Register button (Component not really required)
+	// Components on this page
+	// 1. Timeline
+	// 2. Cards (Take from an earlier implementation if possible, else build from scratch)
+	// 3. Register button (Component not really required)
 
-    render() {
-        return (
-            <>
-                <div>Recruitment SIG page here {this.state.sigName}</div>
-                 <Timeline/>
-                {/* component for sig desc 
+	render() {
+		return (
+			<>
+				<div>Recruitment SIG page here {this.state.sigName}</div>
+				<Timeline rounds={dummy.rounds} />
+				{/* component for sig desc 
 				props: signame and description
 			*/}
-           
-                <SigDescription
-                    sig={this.state.sigName}
-                    desc={this.state.data.desc}
-                />
-                {/* 
+
+				<SigDescription sig={this.state.sigName} desc={this.state.data.desc} />
+				{/* 
 				component for registration butt  
 				props : link (link of Gform)
 			*/}
-                <RegisterButton link="" />
-            </>
-        );
-    }
+				<RegisterButton link="" />
+			</>
+		);
+	}
 }
 export default RecsSIGComponent;
