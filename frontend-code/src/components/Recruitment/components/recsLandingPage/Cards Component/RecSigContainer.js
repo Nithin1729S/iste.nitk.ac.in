a97e7f230f29103ac463b35dadfcf96cdcfb7ae4
ArjunAnilPillai <arjunanilpillai@gmail.com>
2021-08-10 19:27:22 +0530

    Moving components

--- frontend-code/src/components/Recruitment/components/recsLandingPage/Cards Component/RecSigContainer.js
+++ frontend-code/src/components/Recruitment/components/recsLandingPage/Cards Component/RecSigContainer.js
@@ -6,19 +6,13 @@ import './css/sigCards.css';
 import { baseUrl } from '../../../../../constants';
 
 const RecSigCards = (props) => {
-	const renderedList = props.cardList.map(({ name, avatar, summary }) => {
-		const isSig = !Array.isArray(summary);
-		let summaryText = isSig
-			? summary
-			: summary.reduce((allSmps, item) => `${item}\n${allSmps}`, '');
-
+	const renderedList = props.cardList.map(({ name, avatar }) => {
 		const linkOuter = '/recs/';
 		return (
 			<RecSigCard
 				key={name}
 				name={name}
 				avatar={`${baseUrl}${avatar}`}
-				summary={summaryText}
 				buttonLink={`${linkOuter}${name}`}
 			/>
 		);
