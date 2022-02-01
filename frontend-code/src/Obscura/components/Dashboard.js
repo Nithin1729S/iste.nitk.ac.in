fe4de19f84029401bf41656b0c7a391d1ca3b42e
Mansi Singh <psingh7274@gmail.com>
2022-02-01 13:31:06 +0530

    updated user dashboard ui

--- frontend-code/src/Obscura/components/Dashboard.js
+++ frontend-code/src/Obscura/components/Dashboard.js
@@ -28,67 +28,78 @@ class Dashboard extends Component {
       <>
         <Container>
           <Name>Hello { this.state.userName } </Name>
-          
-          <Link to="/obscura/leaderboard">Leaderboard</Link>
-          <div className="container">
-            <div className="row">
+          {/* <Link to="/obscura/leaderboard">Leaderboard</Link> */}
+            <div className="row mt-3">
               {/* year 1 card is never disabled  */}
+              <div className='col l3 m6 s12'>
               <YearCard
                 title="Year 1"
                 link="/obscura/year1"
                 score={this.state.scores[0]}
+                year={1}
               />
-              
+              </div>
+              <div className='col l3 m6 s12'>
               { this.state.yearPassed >= 2 ? 
                 <YearCard
                   title="Year 2"
                   link="/obscura/year2"
                   score= {this.state.scores[1]}
+                  year={2}
                 /> :
-
                 <YearCard
                   title="Year 2"
                   link="/obscura/dashboard"
                   disabled={true}
+                  year={2}
                 /> 
               }
-            </div>
-            <div className="row">
+              </div>
+              <div className='col l3 m6 s12'>
               { this.state.yearPassed >= 3 ? 
                 <YearCard
                   title="Year 3"
                   link="/obscura/year3"
                   score= {this.state.scores[2]}
+                  year={3}
                 /> : 
                 <YearCard
                   title="Year 3"
                   link="/obscura/dashboard"
                   disabled= {true}
+                  year={3}
                 />
               }
+              </div>
+              <div className='col l3 m6 s12'>
               { this.state.yearPassed === 4 ? 
                 <YearCard
                   title="Year 4"
                   link="/obscura/year4"
                   score= {this.state.scores[3]}
+                  year={4}
                 /> : 
                 <YearCard
                   title="Year 4"
                   link="/obscura/dashboard"
                   disabled = {true}
+                  year={4}
                 />
               }
-              
+              </div>
             </div>
-          </div>
+            <Link to="/obscura/leaderboard">
+              <StyledButton className='btn btn-large'>
+                <i className="large material-icons" style={{marginLeft: "-40px", marginTop: "-25px"}}>star</i>
+                Leaderboard
+              </StyledButton>
+            </Link>
         </Container>
       </>
     );
   }
 
 }
-
-
 
 const Container = styled.div`
   padding : 100px;
@@ -109,4 +120,17 @@ const Container = styled.div`
 const Name = styled.h1`
   color : #fff !important;
 `;
+
+const StyledButton = styled.button`
+  background-color: #12a389 !important;
+  font: Roboto;
+  font-size: 2rem;
+  text-transform: capitalize;
+  font-weight: 500;
+  i{
+    position: absolute;
+    color: #FFD700;
+    text-shadow: 0px 0px 15px #FFD700;
+  }
+`
 export default Dashboard;
--- frontend-code/src/Obscura/components/YearCard.js
+++ frontend-code/src/Obscura/components/YearCard.js
@@ -2,29 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const YearCard = ({ title, link, score, disabled }) => {
+const YearCard = ({ title, link, score, disabled, year }) => {
 
   const PassedCard = (
     <Link to={ `${link}` }>
-    <Card passed edclassName="card" >
+    <Card passed className="card center-align" >
       <div className="card-content">
         <div className="card-title">
           <h3>{title}</h3>
+          <hr/>
         </div>
-          <h5>Score: { score }</h5>
-          <h6>Passed</h6>
+        <div>
+        {
+          year===1 || year===4 ? <Triangle /> : year===2 ? <Circle /> : <Rectangle/> 
+        }
+        </div>
+        <div className='row valign-wrapper' style={{marginTop: "150px"}}>
+          <div className='col s6'><h5>Score: { score }</h5></div>
+          <div className='col s6 '><h6>Passed</h6></div>
+        </div>
       </div>
     </Card>
   </Link>
   );
   const DisabledCard = (
+    <>
     <Card disabled={true} className="card">
-      <div className="card-content">
+      <i className="large material-icons">lock_outline</i>
+      <div className="card-content" style={{filter: "blur(2px)"}}>
         <div className="card-title">
           <h3>{title}</h3>
+          <hr/>
+        </div>
+        <div>
+        {
+          year===1 || year===4 ? <Triangle /> : year===2 ? <Circle /> : <Rectangle/> 
+        }
+        </div>
+        <div className='row valign-wrapper' style={{marginTop: "150px"}}>
+          <div className='col s6'><h5>Score: </h5></div>
+          <div className='col s6 '><h6>Passed</h6></div>
         </div>
       </div>
     </Card>
+    </>
   );
 
   return (
@@ -35,11 +56,9 @@ const YearCard = ({ title, link, score, disabled }) => {
 };
 
 const Card = styled.div`
-  max-width: 550px;
-  height: 80%;
+  min-width: 250px;
+  height: 380px;
   width: 100%;
-  padding: 64px;
-  margin: 0px 16px;
   background-color: #262626;
   border-radius: 8px;
   box-shadow: 9px 4px 32px 0px #000000b3;
@@ -56,6 +75,68 @@ const Card = styled.div`
     color : green;
     text-align: center;
   }
+  hr{
+    background-color: #12a389 !important;
+    box-shadow: 9px 4px 32px 0px #12a389;
+    border-color: #12a389;
+    height: 1px;
+  }
+  i{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+    border-radius: 50%;
+    color: #ffffff;
+    background-color: #262626;
+    padding: 5px;
+  }
+`
+const Triangle = styled.div`
+  margin-top: -75px;
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  overflow: hidden;
+  box-shadow: 0 16px 10px -17px #e83e7d;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  &:after{
+    content: "";
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    background: #e83e7d;
+    transform: rotate(45deg);
+    top: 150px;
+    left: 50px;
+    box-shadow: -1px -1px 10px -2px #e83e7d;
+  }
+`
+
+const Circle = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #e83e7d;
+  box-shadow: 0px 0px 15px #e83e7d;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+const Rectangle = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #e83e7d;
+  box-shadow: 0px 0px 15px #e83e7d;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `
 
 export default YearCard;
