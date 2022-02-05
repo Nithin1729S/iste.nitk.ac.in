11baa06769da021ad349596d9a139cde035f77c5
ArjunAnilPillai <arjunanilpillai@gmail.com>
2022-02-06 00:01:57 +0530

    Backend fix

--- backend-code/obscura/admin.py
+++ backend-code/obscura/admin.py
@@ -1,3 +1,21 @@
+from calendar import c
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(team)
+class teamadmin(admin.ModelAdmin):
+    list_display = ("username", 'password', 'yearPassed', 'total_score')
+
+
+@admin.register(score)
+class scoreadmin(admin.ModelAdmin):
+    list_display = ("username", 'firstYear', 'secondYear', 'thirdYear', 'fourthYear', 'firstYearQuestion','secondYearQuestion', 'thirdYearQuestion', 'fourthYearQuestion')
+
+@admin.register(attempt)
+class attemptadmin(admin.ModelAdmin):
+    list_display = ('username', 'firstYear', 'secondYear', 'thirdYear', 'fourthYear')
+
+@admin.register(question)
+class questionamin(admin.ModelAdmin):
+    list_display = ('username', 'firstYear', 'secondYear', 'thirdYear', 'fourthYear')
