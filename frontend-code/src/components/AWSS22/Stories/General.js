7b2eafa03dbb244378ecff5862f309b0fe312209
ArjunAnilPillai <arjunanilpillai@gmail.com>
2022-04-06 10:31:56 +0530

    Minor fixes

--- frontend-code/src/components/AWSS22/Stories/General.js
+++ frontend-code/src/components/AWSS22/Stories/General.js
@@ -1,26 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const General = (props) => {
-  return (
-    <div className="container">
-      <Link
-        to="/awss"
-        style={{color: 'black', fontWeight: 'bold', fontSize: 20}}
-      >Back</Link>
-      <h1 className="center-align">{props.heading}</h1>
-      <div style={{fontSize: 30}}>
-        {props.children}
-      </div>
-      <div
-        className="center-align"
-        style={{color: 'black', fontSize: 20}}
-      >
-        This story was written by <strong style={{fontWeight: 'bold'}}>{props.writer}</strong>, a
-        second year <strong style={{fontWeight: 'bold'}}>{props.branch}</strong> BTech student
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="container">
+			<Link
+				to="/awss"
+				style={{ color: 'black', fontWeight: 'bold', fontSize: 20 }}
+			>
+				Back
+			</Link>
+			<h1 className="center-align">{props.heading}</h1>
+			<div style={{ fontSize: 22, wordSpacing: '4px' }}>{props.children}</div>
+			<div className="center-align" style={{ color: 'black', fontSize: 20 }}>
+				This story was written by{' '}
+				<strong style={{ fontWeight: 'bold' }}>{props.writer}</strong>, a second
+				year <strong style={{ fontWeight: 'bold' }}>{props.branch}</strong>{' '}
+				BTech student
+			</div>
+		</div>
+	);
+};
 
 export default General;
