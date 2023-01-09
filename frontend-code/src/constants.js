6cb4d183e5551704c3e9994b95f2aa224f2c6b50
rahulkr66 <rahulkr.191cs239@nitk.edu.in>
2023-01-09 11:48:44 +0530

    Teams & Recs Update : backend revert
    Signed-off-by: rahulkr66 <rahulkr.191cs239@nitk.edu.in>

--- frontend-code/src/constants.js
+++ frontend-code/src/constants.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// export const baseUrl = 'https://iste.nitk.ac.in/api/';
+export const baseUrl = 'https://iste.nitk.ac.in/api/';
 //* the line below is used to test the local database with the current front-end
 //* uncomment the line below and comment the line with baseURL if you want to test the backend that is in dev mode
-export const baseUrl = 'http://localhost:8000';
+// export const baseUrl = 'http://localhost:8000';
 
 export const baseRequest = axios.create({
 	baseURL: baseUrl,
