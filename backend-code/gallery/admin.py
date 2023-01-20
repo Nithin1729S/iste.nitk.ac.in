17fe22df93ca591c8c8aeac392dccedc580983de
unknown <aneesh0224@gmail.com>
2023-01-20 16:37:19 +0530

    added event attribute to gallery image mode

--- backend-code/gallery/admin.py
+++ backend-code/gallery/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-@admin.register(GalleryImages)
+@admin.register(GalleryImage)
 class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('caption','image')
+    list_display = ('caption','imageLink','event')
--- backend-code/gallery/models.py
+++ backend-code/gallery/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from website.helperFunctions import default_gallery_image_path, gallery_image_upload_path
 
 # Create your models here.
-class GalleryImages(models.Model):
+class GalleryImage(models.Model):
     caption= models.CharField(
         default="",
         max_length=100
     )
-    image= models.ImageField(
+    imageLink= models.ImageField(
         default=default_gallery_image_path,
         upload_to=gallery_image_upload_path,
         blank=True
+    )
+    event= models.CharField(
+        default="",
+        max_length=100
     )
 
