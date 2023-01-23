168efe0fd7af31e676d6fee51279ae12cae0f6fd
Adarsh Kishore <adarsh.kishore786@gmail.com>
2023-01-23 21:35:35 +0530

    Gallery updates

--- frontend-code/src/components/PageComponents/GalleryComponent.js
+++ frontend-code/src/components/PageComponents/GalleryComponent.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { baseRequest } from "../../constants";
+
+const DataHeader = (props) => {
+  return (
+      <div>
+          <div className="col l12 s12">
+              <h3>
+                  {props.header}
+              </h3>
+          </div>
+      </div>
+  );
+};
 
 class GalleryComponent extends React.Component {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    console.log("Fetching...");
+    baseRequest.get("/gallery/").then((res) => {
+      this.setState({
+          images: [...this.state.images, ...res.images],
+      });
+    });
+  }
+
   render() {
     return (
       <>
-        Hello
+        <DataHeader header="Gallery" />
       </>
     );
   }
