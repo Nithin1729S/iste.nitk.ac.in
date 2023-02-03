aa681d48366ad598614df48c909bd0f156165f3f
Ayush Mangukia <54385325+ayush-mangukia@users.noreply.github.com>
2023-02-03 22:59:01 +0530

    Fix Table for Gallery Back End

--- backend-code/gallery/models.py
+++ backend-code/gallery/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from website.helperFunctions import default_gallery_image_path, gallery_image_upload_path
 
 # Create your models here.
 class GalleryImage(models.Model):
@@ -8,8 +7,6 @@ class GalleryImage(models.Model):
         max_length=100
     )
     imageLink= models.ImageField(
-        default=default_gallery_image_path,
-        upload_to=gallery_image_upload_path,
         blank=True
     )
     event= models.CharField(
