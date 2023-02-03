6f2264d70ff73275c077db268db175767a5e6890
Ayush Mangukia <54385325+ayush-mangukia@users.noreply.github.com>
2023-02-03 22:41:39 +0530

    Gallery

--- frontend-code/src/components/PageComponents/GalleryComponent.js
+++ frontend-code/src/components/PageComponents/GalleryComponent.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { baseRequest } from "../../constants";
-// Import the Cloudinary classes. 
-import {fill} from "@cloudinary/url-gen/actions/resize";
-import {CloudinaryImage} from '@cloudinary/url-gen';
-import { AdvancedImage } from '@cloudinary/react';
 
 
 const DataHeader = (props) => {
@@ -21,38 +17,38 @@ const DataHeader = (props) => {
 class GalleryComponent extends React.Component {
   state = {
     images: [
-      {
-        caption: "This is a test",
-        imageLink: "../logo.png",
-        event: "Rahul OnlyFans"
-      },
-      {
-        caption: "This is a test"
-      },
-      {
-        caption: "This is a test"
-      },
-      {
-        caption: "This is a test"
-      },
-      {
-        caption: "This is a test"
-      },
-      {
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      },{
-        caption: "This is a test"
-      }
+//       {
+//         caption: "This is a test",
+//         imageLink: "../logo.png",
+//         event: "Rahul OnlyFans"
+//       },
+//       {
+//         caption: "This is a test"
+//       },
+//       {
+//         caption: "This is a test"
+//       },
+//       {
+//         caption: "This is a test"
+//       },
+//       {
+//         caption: "This is a test"
+//       },
+//       {
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       },{
+//         caption: "This is a test"
+//       }
     ]
   }
 
@@ -64,10 +60,6 @@ class GalleryComponent extends React.Component {
             <div className="col">
               <div className="card center" style={{width: '45vw', padding: 5}}>
                 <figure>
-                  <AdvancedImage 
-                    style={{width: '30vw'}} 
-                    cldImg={new CloudinaryImage('sample', {cloudName: 'dlbqbd5pe'}).resize(fill().width(150).height(150))} 
-                  />
                   <figcaption>{image.caption}</figcaption>
                 </figure>
               </div>
@@ -81,12 +73,12 @@ class GalleryComponent extends React.Component {
   componentDidMount() {
     console.log("Fetching...");
     
-    // baseRequest.get("/gallery/").then((res) => {
-    //   this.setState({
-    //       images: [...this.state.images, ...res.images],
-    //   });
-    // });
-    // this.initialize();
+    baseRequest.get("/gallery/").then((res) => {
+      this.setState({
+          images: [...this.state.images, ...res.images],
+      });
+    });
+    this.initialize();
   } 
 
   render() {
