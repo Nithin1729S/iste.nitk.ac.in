a0bc1b0f9f3548942b66a4fa8c680ab95123ddfa
unknown <aneesh0224@gmail.com>
2023-02-13 01:15:29 +0530

    added endpoint event

--- backend-code/gallery/urls.py
+++ backend-code/gallery/urls.py
@@ -4,5 +4,6 @@ from gallery import views
 app_name = 'team'
 urlpatterns = [
     path('', views.indexView, name='index'),
+    path('event/',views.eventIndex,name='event'),
     path('socialInitiative/',views.socialView,name='social')
 ]
--- backend-code/gallery/views.py
+++ backend-code/gallery/views.py
@@ -3,8 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from gallery.models import GalleryImage
 from gallery.serializers import GallerySerializer
-from django.db.models import DateTimeField
-from django.db.models.functions import Trunc
 
 @api_view(['GET'])
 def indexView(request):
@@ -12,6 +10,33 @@ def indexView(request):
     images_obj = GalleryImage.objects.all().order_by('-date').values()
     images_data = GallerySerializer(images_obj,many=True).data
 
+    print(images_obj[0]['caption'])
+
+    return Response({'images':images_data})
+
+@api_view(['GET'])
+def eventIndex(request):
+    
+    images_obj = GalleryImage.objects.all().order_by('-date').values()
+
+    event_dict={}
+    images_data=[]
+    for img_obj in images_obj:
+        res=event_dict.get(img_obj['event'])
+        
+        if(res is None):
+            ind=len(images_data)
+            event_dict[img_obj['event']]=ind
+            images_data.append({
+                "event":img_obj['event'],
+                "imageLinks":[img_obj['imageLink']]
+            })
+            print(images_data)
+        else:
+            ind=res
+            images_data[ind]["imageLinks"].append(img_obj['imageLink'])
+            print(images_data)
+   
     return Response({'images':images_data})
 
 
