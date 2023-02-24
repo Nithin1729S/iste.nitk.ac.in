8efd007866048fa73e85be1fd94ca60d5f7a07a1
rahulkr66 <rahulkr.191cs239@nitk.edu.in>
2023-02-24 23:47:16 +0530

    Gallery done
    Signed-off-by: rahulkr66 <rahulkr.191cs239@nitk.edu.in>

--- frontend-code/src/components/Gallery/EventCarousel.js
+++ frontend-code/src/components/Gallery/EventCarousel.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import styled from 'styled-components';
 import { Carousel } from 'react-responsive-carousel';
 import './constants/gallery-carousel.css';
 
 const EventCarousel = ({ eventName, links }) => {
+    console.log(eventName)
     return (
-        <>
-            <Carousel infiniteLoop dynamicHeight={ true }>
-                
+        <CarouselFrame>
+            <Carousel className="eventCarousel" infiniteLoop dynamicHeight={ true }>
+                { links.map((link) => {
+                    return (
+                        <div>
+                            <img src={ link } alt={ eventName} loading="lazy" />
+                            <p className="legend">{eventName}</p>
+                        </div>
+                    )
+                })}
             </Carousel>
-        </>
+            
+        </CarouselFrame>
     )
 }
+
+const CarouselFrame = styled.div`
+    padding : 20px;
+    background:
+        linear-gradient(to right, #708090 4px, transparent 4px) 0 0,
+        linear-gradient(to right, #708090 4px, transparent 4px) 0 100%,
+        linear-gradient(to left, #708090 4px, transparent 4px) 100% 0,
+        linear-gradient(to left, #708090 4px, transparent 4px) 100% 100%,
+        linear-gradient(to bottom, #708090 4px, transparent 4px) 0 0,
+        linear-gradient(to bottom, #708090 4px, transparent 4px) 100% 0,
+        linear-gradient(to top, #708090 4px, transparent 4px) 0 100%,
+        linear-gradient(to top, #708090 4px, transparent 4px) 100% 100%;
+
+        background-repeat: no-repeat;
+        background-size: 60px 60px;
+
+`
 
 export default EventCarousel
--- frontend-code/src/components/Gallery/index.js
+++ frontend-code/src/components/Gallery/index.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { baseRequest } from '../../constants';
 
-
+import styled from 'styled-components'
 
 import EventCarousel from './EventCarousel';
 
@@ -15,22 +15,7 @@ const DataHeader = props => {
 
 class GalleryComponent extends React.Component {
     state = {
-        images: [
-            {
-                "event": "tested",
-                "imageLinks": [
-                    'https://res.cloudinary.com/iste-nitk-website/image/upload/v1675586733/Gallery/pics_2022/To_be_honest_kcq7tv.png',
-                    'https://res.cloudinary.com/iste-nitk-website/image/upload/v1675586732/Gallery/pics_2022/Obscura_mlbhy5.png'
-                ]            
-            },
-            {
-                "event": "default",
-                "imageLinks": [
-                    "https://res.cloudinary.com/dmxcn1knn/image/upload/v1675444601/Gallery/Blueprint.png"
-                ]
-            }
-        ],
-        events: ["tested", "default"]
+        eventData: []
     };
 
     componentDidMount() {
@@ -39,81 +24,47 @@ class GalleryComponent extends React.Component {
         baseRequest.get('/gallery/event/').then(res => {
             console.log(res.data.images);
             this.setState({
-                // images: [...this.state.images, ...res.data.images],
-                events : res.data.images
+                eventData: res.data.images
             });
         });
     }
-    Loading() { 
+    Loading() {
         return <h2>🌀 Loading...</h2>;
     }
-
-    getImageArray(eventData) {
-        console.log(eventData)
-        console.log(eventData.imageLinks)
-        let imageLinks = eventData.imageLinks
-        console.log(imageLinks.length)
-        for (const imageLink of imageLinks) {
-            console.log(imageLink)
-        }
-        // for (let image of eventData.imageLinks) {
-        //     console.log(image);
-        //     let cards = [];
-        //     cards.push(
-        //         <div className='image-box'>
-        //             <img src={ image } alt={ eventData.event } loading="lazy" />
-        //         </div>
-        //     );
-        //     // for (let link of image.imageLinks) {
-        //     // }
-        //     return cards;
-        //     // if (image.event === event) {
-        //     // }
-        // }
-        return [];
-    }
     render() {
-        // let res = []
-        // for (let event of this.state.events) {
-        //     console.log(event)
-        //     res.push(
-        //         <div className="row">
-        //             <div className="col s6 push-s3 main-box">
-        //                 <Carousel infiniteLoop dynamicHeight={ true }>
-        //                     { this.getImageArray(event) }
-        //                 </Carousel>
-        //                 <span>{event}</span>
-        //             </div>
-        //         </div>
-        //     );
-        // }
-
         return (
             <div>
                 <DataHeader header="Gallery" />
                 <Suspense fallback={ <this.Loading /> }>
+
                     {
                         this.state.eventData.length === 0 ?
                             null :
-                            this.state.eventData.map((index, eventData) => {
-                                const {event,imageLinks} = eventData
+                            this.state.eventData.map((eventData, index) => {
+                                const { event, imageLinks } = eventData
                                 return (
-                                        <div className="row" key={index}>
-                                            <div className="col s6 push-s3 main-box">
-                                                <EventCarousel 
-                                                    eventName={ event }
-                                                    links = {imageLinks}
-                                                />
-                                                <span>{event}</span>
-                                            </div>
+                                    <CarouselContainer className="row" key={ index }>
+                                        <div className="col s6 push-s3">
+                                            <EventCarousel
+                                                eventName={ event }
+                                                links={ imageLinks }
+                                            />
                                         </div>
+                                    </CarouselContainer>
                                 )
-                        })   
+                            })
                     }
+
                 </Suspense>
             </div>
         );
     }
 }
+
+const CarouselContainer = styled.div`
+    margin : 65px 0 50px 0; 
+`
+
+
 
 export default GalleryComponent;
