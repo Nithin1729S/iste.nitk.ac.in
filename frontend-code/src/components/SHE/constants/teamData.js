f0401562249e3a98ff456ccf5fc4aa443b44a019
rahulkr66 <rahulkr.191cs239@nitk.edu.in>
2023-02-25 12:02:58 +0530

    SHE component added in galery
    Signed-off-by: rahulkr66 <rahulkr.191cs239@nitk.edu.in>

---
+++ frontend-code/src/components/SHE/constants/teamData.js
@@ -0,0 +1,24 @@
+const teamData = [
+    {
+        "first_name" : "Andrea",
+        "last_name" : "Thomas",
+        "image" : "https://iste.nitk.ac.in/api//media/profile_pictures/677_Andrea.JPG"
+    },
+    {
+        "first_name" : "Malvika",
+        "last_name" : "Koushik",
+        "image" : "https://iste.nitk.ac.in/api//media/profile_pictures/700_Malvika.jpg"
+    },
+    {
+        "first_name" : "Shalaka",
+        "last_name" : "Shankar",
+        "image" : "https://iste.nitk.ac.in/api//media/profile_pictures/709_Shalaka.JPG"
+    },
+    {
+        "first_name" : "Vanessa",
+        "last_name" : "Fernandes",
+        "image" : "https://iste.nitk.ac.in/api//media/profile_pictures/710_Vanessa.jpg"
+    }
+]
+
+export default teamData
