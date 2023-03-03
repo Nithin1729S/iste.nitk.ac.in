efc9daa895b9ac6ff51307225f38990d46bcaf29
Ayush Mangukia <54385325+ayush-mangukia@users.noreply.github.com>
2023-03-03 15:15:39 +0530

    Image Changes

--- backend-code/team/views.py
+++ backend-code/team/views.py
@@ -25,7 +25,7 @@ def indexView(request):
                 "id": 587,
                 "first_name": "Shashi",
                 "last_name": "Bushan Arya",
-                "avatar": "/media/profile_pictures/587_Ankit_Stephen_Thomas.jpeg"
+                "avatar": "/media/profile_pictures/586_Shashi_Bushan_Arya.jpeg"
             },
             "role": "Faculty Advisor",
             "description": "Dr. S. B. Arya is the faculty advisor for ISTE NITK and an assistant professor in the Metallurgical and Materials Engineering at NITK. He is an expert in all things corrosion, novel materials developments and metallic passivation. Currently, he is researching in niche and innovative fields such as Li-ion Battery monitoring, corrosion behaviour and steel response to stimuli. Having multiple funded research projects, delivered lectures at several organizations and published 45+ papers, he is more than just a specialist when it comes to his field of study.",
